a42a017aa6c415bc31cba16edc830b848a71560c
The Duskfall Portal Crew <58930427+duskfallcrew@users.noreply.github.com>
2024-12-30 13:29:01 +1300

    Add files via upload
    Fixed a ton of the isssues, still have a bug or fifteen to fix.

--- ui_menus.py
+++ ui_menus.py
@@ -1,10 +1,11 @@
 # ui_menus.py
-from PyQt6.QtWidgets import QMenu
+from PyQt6.QtWidgets import QMenu, QPushButton
 from PyQt6.QtGui import QAction
 from PyQt6.QtCore import Qt
 
 def setup_menus(app):
   menu_bar = app.menuBar()
+  menu_bar.setNativeMenuBar(False) # Fix for the mac menu.
   file_menu = QMenu("File", app)
   menu_bar.addMenu(file_menu)
   
